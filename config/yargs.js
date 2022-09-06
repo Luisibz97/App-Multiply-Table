7516800cbb43b00da81922f4665b623639cb2d59
Luisibz97 <luis_rab_20@hotmail.com>
2022-09-06 19:13:06 -0300

    added Readme

--- config/yargs.js
+++ config/yargs.js
@@ -2,19 +2,21 @@ const argv = require('yargs')
                     .option('b', {
                         alias: 'base',
                         type: 'number',
-                        demandOption: true
+                        demandOption: true,
+                        describe: 'Es la base de la tabla de multiplicar'
                     })
                     .option('l', {
                         alias: 'listar',
                         type: 'boolean',
-                        demandOption: false,
-                        default: false  
+                        default: false,
+                        describe: 'Muestra la tabla en consola'
                     })
                     .option('h', {
                         alias: 'hasta',
                         type: 'number',
                         demandOption: false,
-                        default: 10
+                        default: 10,
+                        describe: 'Este es el numero hasta donde quieras la tabla'
                     })
                     .check( ( argv, options ) => {
                         if ( isNaN( argv.b ) ){
